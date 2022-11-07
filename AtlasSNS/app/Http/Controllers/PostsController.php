7770aa6785440868a9c90125161aa7512ab3ca80
柏倉希奈 <>
2022-11-07 11:40:20 +0900

    PostController.php内にメソッド一つ追加

--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
     public function index(){
         return view('posts.index');
+    }
+
+    public function createForm()
+    {
+        return view('posts.createForm');
     }
 }
