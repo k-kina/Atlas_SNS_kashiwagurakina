e213a2cd2493070affa6d0ec5101ec3d59b75612
柏倉希奈 <>
2022-11-22 23:26:26 +0900

    ログアウトの実装

--- AtlasSNS/app/Http/Controllers/Auth/LoginController.php
+++ AtlasSNS/app/Http/Controllers/Auth/LoginController.php
@@ -51,4 +51,10 @@ class LoginController extends Controller
         }
         return view("auth.login");
     }
+
+    public function logout(){
+        Auth::logout();
+        return redirect('/login');
+    }
+
 }
