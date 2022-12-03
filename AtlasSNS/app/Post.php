e7982a4bc3f6e5929cbcfc368429a76bf84a4d1e
柏倉希奈 <>
2022-12-03 13:39:20 +0900

    リレーション機能追加

--- AtlasSNS/app/Post.php
+++ AtlasSNS/app/Post.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Post extends Model
 {
-    //
+    //ユーザーが投稿した内容を取得
+    //リレーション1対多の1側なので単数系
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
