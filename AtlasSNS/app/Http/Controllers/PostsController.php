88660b64f6fac45161fd00a91fd3b31bacd60c55
柏倉希奈 <>
2022-12-03 13:40:13 +0900

    投稿機能追加

--- AtlasSNS/app/Http/Controllers/PostsController.php
+++ AtlasSNS/app/Http/Controllers/PostsController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class PostsController extends Controller
 {
-    public function index(){
-        return view('posts.index');
-    }
-
-    public function createForm()
+    //トップページ
+    public function index()
     {
-        return view('posts.createForm');
+        $user = Auth::user();//ログインしたユーザー情報の取得
+        //dd($user);
+        $post = \DB::table('posts')->get();//postsテーブルから全てのレコード情報取得
+        //dd($post);
+        return view('posts.index',['post'=> $post,'user'=> $user]);
+    }//viewで表示する
+
+    //投稿機能
+    public function create(Request $request)
+    {
+        $id = Auth::id();//ログインユーザーidの取得
+        $post = $request->input('newPost');//つぶやきを取得
+        \DB::table('posts')->insert([//postテーブルに送る
+            'post' => $post,//postカラムを$postに入れる
+            'user_id' => $id//user_idカラムを$idに入れる
+        ]);
+
+        return redirect('/top');
     }
 }
