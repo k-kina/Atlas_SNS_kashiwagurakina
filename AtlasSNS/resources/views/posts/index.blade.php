b72421a44f122e7c1900224b6a748b3f2f187b30
柏倉希奈 <>
2022-12-03 13:39:55 +0900

    投稿内容の表示

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -2,8 +2,29 @@
 
 @section('content')
 
-<p class="pull-right"><a class="btn btn-success" href="post/create-form">投稿する</a></p>
+    <div class="container">
 
-<h2>機能を実装していきましょう。</h2>
+    <p class="page-header"><img src="images/icon1.png"width="50"height="50"></p>
+        {!! Form::open(['url' => 'post/create']) !!}
+
+        <div class="form-group">
+            {!! Form::input('text', 'newPost', null, ['required', 'class' => 'form-control', 'placeholder' => '投稿内容を入力してください。']) !!}
+             </div>
+
+        <button type="submit"></button><img src="images/post.png"width="100"height="100">
+
+
+        {!! Form::close() !!}
+
+        {{ $user->user->username }}
+
+        @foreach ($post as $post)
+        <tr>
+            <td>{{ $post->post}}</td><!-- 投稿内容 -->
+            <td>{{ $post->created_at}}</td><!--登録日 -->
+        </tr>
+        @endforeach
+
+    </div>
 
 @endsection
