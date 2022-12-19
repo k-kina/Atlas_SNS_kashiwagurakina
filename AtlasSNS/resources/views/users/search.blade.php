90a74cd277876005196435238f845ae949bea8e1
柏倉希奈 <>
2022-12-19 12:33:04 +0900

    検索機能記述

--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 
+<form action="/search" method="get">
+  <input type="search" placeholder="ユーザー名" name="search">
+  <button type="submit">検索</button>
+</form>
+
+@foreach ($search as $search)
+<tr>
+  <td>{{ $search->username}}</td>
+</tr>
+@endforeach
+
 
 
 @endsection
