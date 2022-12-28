193f03dae98fd3872e9e3a91153414d142b60b5f
柏倉希奈 <>
2022-12-28 20:53:21 +0900

    フォローフォロワー機能追加

--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -5,11 +5,29 @@
 <form action="/search" method="get">
   <input type="search" placeholder="ユーザー名" name="search">
   <button type="submit">検索</button>
+
+  <p>検索ワード：{{ $keyword }}</p>
 </form>
 
 @foreach ($search as $search)
 <tr>
   <td>{{ $search->username}}</td>
+
+  @if (auth()->user()->isFollowing($search->id))
+  <form action="/unFollow" method="POST">
+  <input type="hidden" name="id" value="{{$search->id}}">
+   {{ csrf_field() }} <!--CSRF対策 -->
+ <button type="submit" class="btn btn-danger">フォロー解除</button>
+
+</form>
+@else
+<form action="/follow" method="POST">
+    <input type="hidden" name="id" value="{{$search->id}}"><!--hiddenを使用しブラウザにはフォローする人のidが表示されないようにデータを送る初期値は$search->id-->
+    {{ csrf_field() }}<!-- CSRF対策 -->
+<button type="submit" class="btn btn-primary">フォローする</button>
+ </form>
+@endif
+
 </tr>
 @endforeach
 
