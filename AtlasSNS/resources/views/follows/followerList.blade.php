e5aeaf0481ce6ecab5480148967011d2dae28c6d
柏倉希奈 <>
2023-01-05 14:12:24 +0900

    followerListの追加

--- AtlasSNS/resources/views/follows/followerList.blade.php
+++ AtlasSNS/resources/views/follows/followerList.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.login')
 
 @section('content')
+<div class="container">
+  <h1>Follower List</h1>
+  <div class="list-images">
+
+  @foreach($posts as $post)
+<a href="/otherProfile2"><img src="images/icon2.png"width="50"height="50"></a>
+<p>{{ $post->user->username }}</p>
+<p>{{ $post->post }}</p>
+@endforeach
+
+  </div>
+</div>
 @endsection
