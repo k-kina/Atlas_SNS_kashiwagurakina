90facfc435f5902f3f0c12d0ea54b3b82445ff10
柏倉希奈 <>
2023-01-05 14:11:37 +0900

    フォローフォロワーリストの記述

--- AtlasSNS/app/Http/Controllers/FollowsController.php
+++ AtlasSNS/app/Http/Controllers/FollowsController.php
@@ -3,14 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Post;
 class FollowsController extends Controller
 {
-    //
-    public function followList(){
-        return view('follows.followList');
+    //フォローしてくれているユーザーの投稿を表示
+    public function followerList()
+    {
+        $following_id = Auth::user()->follows()->pluck('following_id');
+        //dd($following_id); // フォローしてくれているユーザーのidを取得し$following_idに格納する
+
+        $posts = Post::with('user')->whereIn('user_id', $following_id)->get();// $following_idに格納したユーザーidと一致するカラム名：user_id(投稿内容)を取得
+        //dd($posts);
+
+        return view('follows.followerList', compact('posts'));
     }
-    public function followerList(){
-        return view('follows.followerList');
-    }
+
+    //フォローしているユーザーの投稿を表示
+    public function followList()
+    {
+        $following_id = Auth::user()->follows()->pluck('followed_id');
+        //dd($following_id); // フォローしているユーザーのidを取得し$following_idに格納する
+
+        $posts = Post::with('user')->whereIn('user_id', $following_id)->get();// $following_idに格納したユーザーidと一致するカラム名：user_id(投稿内容)を取得
+        //dd($posts);
+
+        return view('follows.followList', compact('posts'));
+}
 }
