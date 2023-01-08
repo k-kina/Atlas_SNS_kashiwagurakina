b6a28d296d76a76f1573aa5761dc2215cac3f8ea
柏倉希奈 <>
2023-01-09 01:36:51 +0900

    フォロー機能追加

--- AtlasSNS/app/Http/Controllers/UsersController.php
+++ AtlasSNS/app/Http/Controllers/UsersController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 use App\User;
 
+use Illuminate\Support\Facades\Auth;
+
+use App\Follow;
+
 class UsersController extends Controller
 {
     //
@@ -20,7 +24,7 @@ class UsersController extends Controller
         $search = \DB::table('users')->get();
         //dd($search);
         return view('users.search',['search'=>$search]);
-    }
+    }   //テーブルから値を$search変数に入れて、viewにsearchを変数として渡す
 
     //検索結果の表示
     public function usersearch(Request $request)
@@ -32,16 +36,38 @@ class UsersController extends Controller
 
         if (!empty($keyword)){//$keywordで値を受け取った場合if文の中で取得するデータの絞り込み
             //dd($keyword);
-                $query->where('username', 'like', '%{$keyword}%');//usernameカラムから$keywordと一致するデータを全て取得(部分一致)し、$queryとして保持される
+                $query->where('username', 'like', "%{$keyword}%");//usernameカラムから$keywordと一致するデータを全て取得(部分一致)し、$queryとして保持される
                 //dd($query);
 
         }
-        //\DB::table('users')
-        //->where('username',$search)//usernameカラムから$searchと一致するものを取得
+
         $search = $query->get();//上記で取得した$queryを変数$searchに代入
         //dd($search);
 
         return view('users.search', compact('search', 'keyword'));
     }   //viewにkeywordとsearchを変数として渡す
 
+    //フォロー機能
+    public function follow(Request $request)
+    {
+        $id = Auth::id();
+        $followedId = $request->input('id');//フォローする人のIDを受け取り$followedIdに格納
+        //dd($followedId);
+        \DB::table('follows')->insert([//followsテーブルにデータを登録する
+            'following_id' => $id,//$idをfollowing_idカラムに代入する
+            'followed_id' => $followedId//$followedIdをfollowed_idカラムに代入する
+        ]);
+        return back();
+       }
+
+   // フォロー解除
+   public function unFollow(Request $request)
+   {
+        $id = Auth::id();
+        $followedId = $request->input('id');//フォローした人のIDを受け取り$followedIdに格納
+        //dd($followedId);
+        Follow::where('following_id',$id)->where('followed_id',$followedId)->delete();//解除
+
+        return back();//解除したら戻る
+    }
 }
